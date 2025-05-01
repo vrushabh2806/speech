476c9110b4d8d53c73d57cd62f837f0830024ba8
22IT124_Vrushabh <166047640+vrushabh2806@users.noreply.github.com>
2025-05-01 13:44:49 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Vrushabh Pawar\\Downloads\\flutter_windows_3.27.1-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Vrushabh Pawar\\Documents\\SEM6\\MAD Mini project 1\\quiz_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Vrushabh Pawar\\Downloads\\flutter_windows_3.27.1-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Vrushabh Pawar\\Documents\\SEM6\\MAD Mini project 1\\quiz_app"
+  "FLUTTER_ROOT=C:\\Users\\Vrushabh Pawar\\Downloads\\flutter_windows_3.27.1-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Vrushabh Pawar\\Documents\\SEM6\\MAD Mini project 1\\quiz_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Vrushabh Pawar\\Documents\\SEM6\\MAD Mini project 1\\quiz_app"
+  "FLUTTER_TARGET=C:\\Users\\Vrushabh Pawar\\Documents\\SEM6\\MAD Mini project 1\\quiz_app\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Vrushabh Pawar\\Documents\\SEM6\\MAD Mini project 1\\quiz_app\\.dart_tool\\package_config.json"
+)
